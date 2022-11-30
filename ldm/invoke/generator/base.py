89c6fbff495716d3be734243b4f8cffe4a8675e2
Kevin Turner <83819+keturn@users.noreply.github.com>
2022-11-30 15:25:47 -0800

    🚧 post-rebase repair

--- ldm/invoke/generator/base.py
+++ ldm/invoke/generator/base.py
@@ -181,8 +181,6 @@ class Generator:
         matched_result.paste(init_image, (0,0), mask = multiplied_blurred_init_mask)
         return matched_result
 
-
-
     def sample_to_lowres_estimated_image(self,samples):
         # origingally adapted from code by @erucipe and @keturn here:
         # https://discuss.huggingface.co/t/decoding-latents-to-rgb-without-upscaling/23204/7
